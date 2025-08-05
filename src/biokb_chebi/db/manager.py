b1598a9bad4862f472f568da2e07a230bcbf7062
David Buchmann <david.buchmann@scai.fraunhofer.de>
2025-08-05 12:36:58 +0200

    Added a function set_data_folder() and included it in import_data()

--- src/biokb_chebi/db/manager.py
+++ src/biokb_chebi/db/manager.py
@@ -5,7 +5,7 @@ from typing import Optional
 from sqlalchemy import Engine, create_engine, text
 from sqlalchemy.orm import sessionmaker
 
-from biokb_chebi.constants.basic import DB_DEFAULT_CONNECTION_STR
+from biokb_chebi.constants.basic import DB_DEFAULT_CONNECTION_STR, DATA_FOLDER
 from biokb_chebi.db.importer import DatabaseImporter
 
 logging.basicConfig(level=logging.INFO)
@@ -16,6 +16,7 @@ class DbManager:
     def __init__(
         self,
         engine: Optional[Engine] = None,
+        data_folder: Optional[str] = None,
     ):
         connection_str = os.getenv("CONNECTION_STR", DB_DEFAULT_CONNECTION_STR)
         self.engine = engine if engine else create_engine(str(connection_str))
@@ -25,6 +26,14 @@ class DbManager:
 
         logger.info("Engine %s", self.engine)
         self.Session = sessionmaker(bind=self.engine)
+        self.__data_folder = data_folder or DATA_FOLDER
+
+
+    def set_data_folder(self, path_to_datafolder: str) -> None:
+        """Set the data folder."""
+        self.__data_folder = path_to_datafolder
 
     def import_data(self):
-        return DatabaseImporter(self.engine).import_db()
+        db_importer = DatabaseImporter(engine=self.engine)
+        db_importer.set_data_folder(self.__data_folder)
+        return db_importer.import_db()
