4f69aba8fc7842bdb57e49cc93774ac36b844850
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-25 17:27:26 +0100

    tests improved and working

--- tests/test_chebi.py
+++ tests/test_chebi.py
@@ -1,19 +1,88 @@
-import os
+# TODO: Only runs in the moment with podman, not prepared for docker yet.
+import logging
+import shutil
+import subprocess
+import time
+import zipfile
 from pathlib import Path
 
 import pytest
+from click.testing import CliRunner
+from neo4j import GraphDatabase, Result
+from neo4j.exceptions import ServiceUnavailable
 from sqlalchemy import Engine, create_engine
 
+from biokb_chebi.cli import main
+from biokb_chebi.constants import NEO4J_USER
 from biokb_chebi.db import models
 from biokb_chebi.db.manager import DbManager
+from biokb_chebi.rdf.neo4j_importer import Neo4jImporter
 from biokb_chebi.rdf.turtle import TurtleCreator
+
+logger = logging.getLogger(__name__)
 
 TEST_DIR = Path(__file__).parent
 DATA_DIR = TEST_DIR / "data"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "test_passwd"
 
 engine: Engine = create_engine(
     f"sqlite:///",
 )
+
+container_engine = None
+if shutil.which("podman"):
+    container_engine = "podman"
+elif shutil.which("docker"):
+    container_engine = "docker"
+
+
+def is_neo4j_responsive(
+    uri="bolt://localhost:7687", user=NEO4J_USER, password=NEO4J_PASSWORD
+) -> bool:
+    try:
+        with GraphDatabase.driver(uri, auth=(user, password)) as driver:
+            driver.verify_connectivity()
+        return True
+    except Exception as e:
+        logger.info(f"Wait for Neo4j to be responsive: {e}")
+        return False
+
+
+def start_neo4j_container():
+    subprocess.run(f"{container_engine} rm -f neo4j-test".split(" "))
+    command = f"{container_engine} run -d --name neo4j-test -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH={NEO4J_USER}/{NEO4J_PASSWORD} neo4j:5".split(
+        " "
+    )
+
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        container_id = result.stdout.strip()
+        logger.info(
+            f"Neo4j container started with ID: {container_id}. Waiting for it to be ready..."
+        )
+        # check if the database is ready
+        for _ in range(30):
+            try:
+                if is_neo4j_responsive():
+                    logger.info("Neo4j is responsive and ready to accept connections.")
+                    break
+            except ServiceUnavailable:
+                pass
+            time.sleep(1)
+        return container_id
+    else:
+        logger.error("Error starting container:\n %s", result.stderr)
+        return None
+
+
+def remove_neo4j_container(container_id: str):
+    command = ["podman", "rm", "-f", container_id]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        logger.info(f"Neo4j container {container_id} stopped and removed.")
+    else:
+        logger.error("Error stopping/removing container:\n %s", result.stderr)
 
 
 @pytest.fixture
@@ -61,8 +130,7 @@ class TestTurtleCreation:
 
         assert path.exists()
         assert path.stat().st_size > 0
-        # get list of files in the zip
-        import zipfile
+        # get list of files in the zip file
 
         expected_file_set = {
             "inchi.ttl",
@@ -78,3 +146,76 @@ class TestTurtleCreation:
 
         # Clean up
         path.unlink()
+
+
+class TestNeo4jImport:
+    """Test Neo4j import from Turtle files."""
+
+    def test_import_to_neo4j(
+        self,
+        dbm: DbManager,
+        tc: TurtleCreator,
+    ) -> None:
+        """Check if data is imported into Neo4j."""
+        if container_engine is None:
+            pytest.skip("Neither podman nor docker is available.")
+
+        container_id = start_neo4j_container()
+
+        if container_id is None:
+            pytest.skip("Could not start Neo4j container.")
+
+        dbm.import_data(keep_files=True)
+        path_to_zip_file = tc.create_ttls()
+
+        importer = Neo4jImporter(
+            neo4j_uri="bolt://localhost:7687",
+            neo4j_user=NEO4J_USER,
+            neo4j_pwd=NEO4J_PASSWORD,
+        )
+        importer._set_test_zipped_ttls_path(path_to_zip_file)
+        importer.import_ttls()
+
+        with importer.driver.session() as session:
+            result: Result = session.run("MATCH (n:Compound) RETURN COUNT(n) AS count")
+            record = result.single()
+            assert record is not None
+            count = record["count"]
+            assert count == 10
+
+            result = session.run("MATCH (n:OtherName) RETURN COUNT(n) AS count")
+            record = result.single()
+            assert record is not None
+            count = record["count"]
+            assert count == 9
+
+            result = session.run("MATCH (n:InChI) RETURN COUNT(n) AS count")
+            record = result.single()
+            assert record is not None
+            count = record["count"]
+            assert count == 8
+
+        # Clean up
+        Path(path_to_zip_file).unlink()
+        remove_neo4j_container(container_id)
+
+
+class TestCLI:
+    """Test CLI commands."""
+
+    def test_cli_help(self) -> None:
+        """Check if CLI help works."""
+        runner = CliRunner()
+        # Simuliert den Aufruf: biokb-chebi --help
+        result = runner.invoke(main, ["--help"])
+
+        assert result.exit_code == 0
+        assert "Usage:" in result.output
+        assert "Options:" in result.output
+
+    def test_cli_version(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ["--version"])
+
+        assert result.exit_code == 0
+        assert "0.1.0" in result.output
