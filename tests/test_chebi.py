94f6ac250726c7eb7b2d4796a7cc634583430432
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2026-01-06 19:50:44 +0100

    version increased by 0.0.1

--- tests/test_chebi.py
+++ tests/test_chebi.py
@@ -217,4 +217,4 @@ class TestCLI:
         result = runner.invoke(main, ["--version"])
 
         assert result.exit_code == 0
-        assert "0.1.0" in result.output
+        assert "0.1.1" in result.output
