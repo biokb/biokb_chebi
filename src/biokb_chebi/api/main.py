06cc5220c8c87ec7b4de342d711a12c3bf13b374
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2026-09-17 12:10:30 +0200

    refactor: enhance database engine creation with connection pooling options

--- src/biokb_chebi/api/main.py
+++ src/biokb_chebi/api/main.py
@@ -38,7 +38,7 @@ PASSWORD: str = os.environ.get("CHEBI_API_PASSWORD", "admin")
 
 def get_engine() -> Engine:
     conn_url = os.environ.get("CONNECTION_STR", DB_DEFAULT_CONNECTION_STR)
-    engine: Engine = create_engine(conn_url)
+    engine: Engine = create_engine(conn_url, pool_pre_ping=True, pool_recycle=3600)
     return engine
 
 
